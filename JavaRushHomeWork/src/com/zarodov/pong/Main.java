c24ed146881db3d8f35ff3ddaf94702159f345db
aazarodov <aazarodov@gmail.com>
2016-12-16 15:11:59 +0300

    2016.12.16

---
+++ JavaRushHomeWork/src/com/zarodov/pong/Main.java
@@ -0,0 +1,75 @@
+package com.zarodov.pong;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by zarodov on 16.12.2016.
+ */
+public class Main extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel jContentPane = null;
+    private PanelPelota panel = null;
+
+    private PanelPelota getPanel() {
+        if (panel == null)
+            panel = new PanelPelota();
+        return panel;
+    }
+
+    public Main() {
+        super();
+        initialize();
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                formKeyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                formKeyReleased(e);
+            }
+        });
+    }
+
+    private void initialize() {
+        this.setResizable(false);
+        this.setBounds(new Rectangle(312, 184, 250, 250));
+        this.setMinimumSize(new Dimension(250, 250));
+        this.setMaximumSize(new Dimension(250, 250));
+        this.setContentPane(getJContentPane());
+        this.setTitle("Pong");
+    }
+
+    private void formKeyPressed(KeyEvent e) {
+        panel.keyPressed(e);
+    }
+
+    private void formKeyReleased(KeyEvent e) {
+        panel.keyReleased(e);
+    }
+
+
+    public JPanel getJContentPane() {
+        if  (jContentPane == null) {
+            jContentPane = new JPanel();
+            jContentPane.setLayout(new BorderLayout());
+            jContentPane.add(getPanel(), BorderLayout.CENTER);
+        }
+        return jContentPane;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main thisClass = new Main();
+                thisClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                thisClass.setVisible(true);
+            }
+        });
+    }
+}
